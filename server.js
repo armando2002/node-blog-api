8b144eef3d9ee4aed8d4b5caef1dbd828df2e52d
Stephen Williams <armando2002@gmail.com>
2018-05-29 19:32:04 -0700

    added PUT and DELETE and completed and tested unmodularized app

--- server.js
+++ server.js
@@ -38,6 +38,39 @@ app.post('/blog-posts', jsonParser, (req, res) => {
     res.status(201).json(item);
   });
 
+  // initial delete
+  app.delete('/blog-posts/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog post \`${req.params.id}\``);
+    res.status(204).end();
+  });
+
+  // initial .put
+  app.put('/blog-posts/:id', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author', 'id'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    if (req.params.id !== req.body.id) {
+        const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+        console.error(message);
+        return res.status(400).send(message);
+    }
+    console.log(`Updating blog post \`${req.params.id}\``);
+    BlogPosts.update({
+        id: req.params.id,
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author 
+    });
+    res.status(204).end();
+  });
+
   app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
   });
