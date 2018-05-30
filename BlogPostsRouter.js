74ce513afb4d147b18eec7efb11e475d509fc470
Stephen Williams <armando2002@gmail.com>
2018-05-30 13:29:02 -0700

    modularized and tested

---
+++ BlogPostsRouter.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+app.get('/blog-posts', (req, res) => {
+    res.json(BlogPosts.get());
+  });
+
+// initial .post
+app.post('/', jsonParser, (req, res) => {
+    // ensure `title` 'content' and `author` are in request body
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+  
+    const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+    res.status(201).json(item);
+  });
+
+  // initial delete
+  app.delete('/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog post \`${req.params.id}\``);
+    res.status(204).end();
+  });
+
+  // initial .put
+  app.put('/:id', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author', 'id'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    if (req.params.id !== req.body.id) {
+        const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+        console.error(message);
+        return res.status(400).send(message);
+    }
+    console.log(`Updating blog post \`${req.params.id}\``);
+    BlogPosts.update({
+        id: req.params.id,
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author 
+    });
+    res.status(204).end();
+  });
+
+module.exports = router;
