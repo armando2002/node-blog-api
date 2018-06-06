c08354ca7b6a924d85c0b88b7c8762734e9ff559
armando2002 <armando2002@gmail.com>
2018-06-05 22:16:10 -0700

    added tests for POST and PUT: PUT currently giving 500 error

--- test/test-server.js
+++ test/test-server.js
@@ -33,11 +33,63 @@ describe('Blog Posts', function() {
                 });
             });
     });
+    
     // it should do something on POST
+    it('should add a blog post on POST', function() {
+        const blogPost = {
+            title: "Test blog post",
+            content: "This is a test blog post.",
+            author: "Test Author"
+        };
+
+        const expectedKeys = ['id', 'publishDate'].concat(Object.keys(blogPost));
+    
+
+        return chai.request(app)
+            .post('/blog-posts')
+            .send(blogPost)
+            .then(function(res) {
+                expect(res).to.have.status(201);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.have.all.keys(expectedKeys);
+                expect(res.body.title).to.equal(blogPost.title);
+                expect(res.body.content).to.equal(blogPost.content);
+                expect(res.body.author).to.equal(blogPost.author);
+
+        });
+
+    });
 
     // it should do something on PUT
+    it('should update a blog post on PUT', function() {
+        const updatePost = {
+            id: "9c9b77bf-39a9-44f1-b5ad-22c79aa1b8a8",
+            title: "New Favorite Cat Food",
+            content: "My new favorite cat food is tuna!",
+            author: "Kuroi"
+        }
 
-    // it should do somethign on DELETE
+        const expectedKeys = ['publishDate'].concat(Object.keys(updatePost));
+
+        return chai.request(app)
+            console.log(updatePost.id);
+            .put(`/blog-posts/${updatePost.id}`)
+            .send(updatePost)
+            .then(function(res) {
+                expect(res).to.have.status(204);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('object');
+                expect(resp.body).tp.have.all.keys(expectedKeys);
+                expect(res.body.id).to.equal(updatePost.id);
+                expect(res.body.title).to.equal(updatePost.title);
+                expect(res.body.content).to.equal(updatePost.content);
+                expect(res.body.author).to.equal(updatePost.author);
+
+            });
+
+    });
+    // it should do something on DELETE
 
 
 
