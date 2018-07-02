d46f9bbebb084fddc31bf0b5e9ddf82acd2b046b
Stephen Williams <armando2002@gmail.com>
2018-07-01 18:05:23 -0700

    udpated config.js and models.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/node-blog-api";
+  process.env.DATABASE_URL || "mongodb://localhost/nodeBlogAPIDb";
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://localhost/node-blog-api";
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/nodeBlogAPIDb";
 exports.PORT = process.env.PORT || 8080;
