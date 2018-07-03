8d4e42ed401d90b998b0ee5c415479aa15d36333
Stephen Williams <armando2002@gmail.com>
2018-07-02 19:09:39 -0700

    updating

--- server.js
+++ server.js
@@ -76,7 +76,8 @@ app.post('/blog-posts', (req,res) => {
 app.delete('/blog-posts/:id', (req,res) => {
   BlogPost.findByIdAndRemove(req.params.id)
   .then(() => {
-    res.status(204).json({message: `deleted ${req.params.id}`});
+    res.status(204);
+    res.send({message: `deleted ${req.params.id}`});
   })
   .catch(err => {
     res.status(500).json({ error: 'internal server error'});
